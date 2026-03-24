8dd070e4c630e0395f45785a40fae99f98162c09
itamar levi <leviitamar034@gmail.com>
2026-03-24 11:10:40 +0200

    change installation  path

--- server/src/controllers/shelters.controller.js
+++ server/src/controllers/shelters.controller.js
@@ -1,4 +1,4 @@
-import { getAllShelters } from "../services/shelters.service.js";
+import { getAllShelters, getNearestShelters } from "../services/shelters.service.js";
 
 export async function getShelters (req, res) {
     try {
@@ -15,5 +15,20 @@ export async function getShelters (req, res) {
             success: false, 
             message: "Internal Server Error" 
         });
+    }
+};
+
+export const findNearby = async (req, res) => {
+    try {
+        const { lat, lng } = req.body;
+
+        if (!lat || !lng) {
+            return res.status(400).json({ message: "Latitude and Longitude are required" });
+        }
+
+        const shelters = await getNearestShelters(lat, lng);
+        res.status(200).json(shelters);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 };
--- server/src/services/shelters.service.js
+++ server/src/services/shelters.service.js
@@ -1,4 +1,5 @@
 import { getDB } from "../db/mongo.js";
+
 export async function getAllShelters () {
     try {
         const db = getDB();
@@ -8,3 +9,25 @@ export async function getAllShelters () {
         throw new Error(`failed to fetch shelters: ${error.message}`);
     }
 };
+
+export async function getNearestShelters(lat, lng) {
+    try {
+        const db = getDB();
+        const collection = db.collection('shelters');
+
+        const nearest = await collection.find({
+            location: {
+                $near: {
+                    $geometry: {
+                        type: "Point",
+                        coordinates: [parseFloat(lng), parseFloat(lat)] 
+                    }
+                }
+            }
+        }).limit(15).toArray();
+
+        return nearest;
+    } catch (error) {
+        throw new Error(`Error finding nearest shelters: ${error.message}`);
+    }
+}
